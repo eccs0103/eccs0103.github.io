faf4e8afae2af40fe9066a44c4b15fe713e2f016
eccs0103 <eccs0103@gmail.com>
2023-01-19 10:17:41 +0400

    0.5.1

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-const queryDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-if (queryDarkScheme.matches) {
-	// Theme set to dark.
-} else {
-	// Theme set to light.
-}
-queryDarkScheme.addEventListener(`change`, (event) => {
-
-});

b15a8d4b842de759c7350818960513e16fe33720
eccs0103 <eccs0103@gmail.com>
2023-01-23 12:26:31 +0400

    0.7.0

--- scripts/feed.js
+++ scripts/feed.js
@@ -15,57 +15,62 @@ try {
 		}
 	]));
 	const templatePostStructure = (/** @type {HTMLTemplateElement} */ (document.querySelector(`template#post-structure`)));
-	database.filter((post, index) => filters.every(filter => filter(post, index))).forEach((post, index) => {
-		const articlePost = main.appendChild((/** @type {HTMLElement} */ (/** @type {HTMLElement} */(templatePostStructure.content.querySelector(`article#post-`)).cloneNode(true))));
-		articlePost.id = `post-${index}`;
-		{
-			const h1Title = (/** @type {HTMLHeadingElement} */ (articlePost.querySelector(`h1.title`)));
-			h1Title.innerText = post.title;
-			{ }
-			const timeDate = (/** @type {HTMLTimeElement} */ (articlePost.querySelector(`time.date`)));
-			timeDate.dateTime = post.date.toString();
-			timeDate.innerText = post.date.toLocaleString(undefined, {
-				year: `numeric`,
-				month: `long`,
-				day: `numeric`,
-				hourCycle: `h24`,
-				hour: `2-digit`,
-				minute: `2-digit`,
-			});
-			timeDate.classList.add(`button`);
-			timeDate.addEventListener(`click`, (event) => {
-				navigator.clipboard.writeText(`${location.origin}${location.pathname}#${articlePost.id}`)
-					.then(() => {
-						window.alert(`Ссылка к посту скопирована.`);
-					})
-					.catch((reason) => {
-						throw reason instanceof Error ? reason : new Error(reason);
-					});
-			});
-			{ }
-			const divContent = (/** @type {HTMLDivElement} */ (articlePost.querySelector(`div.content`)));
-			divContent.innerHTML = post.content;
-			{ }
-			const divTags = (/** @type {HTMLDivElement} */ (articlePost.querySelector(`div.tags`)));
+	database.forEach((post, index) => {
+		if (filters.every(filter => filter(post, index))) {
+			const articlePost = main.appendChild((/** @type {HTMLElement} */ (/** @type {HTMLElement} */(templatePostStructure.content.querySelector(`article#post-`)).cloneNode(true))));
+			articlePost.id = `post-${index}`;
 			{
-				post.tags.forEach((tag, index) => {
-					const dfnTag = divTags.appendChild(document.createElement(`dfn`));
-					dfnTag.innerText = tag;
-					if (index != post.tags.length - 1) {
-						dfnTag.replaceWith(dfnTag, document.createTextNode(`, `));
-					}
-					dfnTag.classList.add(`button`);
-					dfnTag.addEventListener(`click`, (event) => {
+				const h1Title = (/** @type {HTMLHeadingElement} */ (articlePost.querySelector(`h1.title`)));
+				h1Title.innerText = post.title;
+				{ }
+				const timeDate = (/** @type {HTMLTimeElement} */ (articlePost.querySelector(`time.date`)));
+				timeDate.dateTime = post.date.toString();
+				timeDate.innerText = post.date.toLocaleString(undefined, {
+					year: `numeric`,
+					month: `long`,
+					day: `numeric`,
+					hourCycle: `h24`,
+					hour: `2-digit`,
+					minute: `2-digit`,
+				});
+				timeDate.classList.add(`button`);
+				timeDate.addEventListener(`click`, (event) => {
+					navigator.clipboard.writeText(`${location.origin}${location.pathname}#${articlePost.id}`)
+						.then(() => {
+							window.alert(`Ссылка к посту скопирована.`);
+						})
+						.catch((reason) => {
+							throw reason instanceof Error ? reason : new Error(reason);
+						});
+				});
+				{ }
+				const divContent = (/** @type {HTMLDivElement} */ (articlePost.querySelector(`div.content`)));
+				divContent.innerHTML = post.content;
+				{ }
+				const divTags = (/** @type {HTMLDivElement} */ (articlePost.querySelector(`div.tags`)));
+				{
+					post.tags.forEach((tag, index) => {
+						const dfnTag = divTags.appendChild(document.createElement(`dfn`));
+						dfnTag.innerText = tag;
+						if (index != post.tags.length - 1) {
+							dfnTag.replaceWith(dfnTag, document.createTextNode(`, `));
+						}
+						dfnTag.classList.add(`button`);
 						const search = location.search.match(/tags=(.*)(,|$)/);
 						const tags = new Set(search ? search[1].split(`,`) : []);
 						if (tags.has(tag)) {
-							tags.delete(tag);
-						} else {
-							tags.add(tag);
+							dfnTag.classList.add(`mark`);
 						}
-						location.search = tags.size == 0 ? `` : `tags=${Array.from(tags).join(`,`)}`;
+						dfnTag.addEventListener(`click`, (event) => {
+							if (tags.has(tag)) {
+								tags.delete(tag);
+							} else {
+								tags.add(tag);
+							}
+							location.search = tags.size == 0 ? `` : `tags=${Array.from(tags).join(`,`)}`;
+						});
 					});
-				});
+				}
 			}
 		}
 	});

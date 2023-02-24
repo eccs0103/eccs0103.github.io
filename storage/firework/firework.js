73610b22910cfd4867c29b6681d85ede296072a0
eccs0103 <eccs0103@gmail.com>
2023-02-24 21:42:16 +0400

    0.8.3
    Signed-off-by: eccs0103 <eccs0103@gmail.com>

--- storage/firework/firework.js
+++ storage/firework/firework.js
@@ -12,10 +12,10 @@ class Engine {
 	 */
 	constructor(handler) {
 		const instance = this;
-		requestAnimationFrame(function repeater(time) {
+		requestAnimationFrame(function callback(time) {
 			instance.#time = time;
 			handler();
-			requestAnimationFrame(repeater);
+			requestAnimationFrame(callback);
 		});
 	}
 	/** @type {DOMHighResTimeStamp} */ #time;
@@ -27,12 +27,15 @@ class Engine {
 const canvas = document.body.appendChild(document.createElement(`canvas`));
 canvas.style.width = `100vw`;
 canvas.style.height = `100vh`;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-window.addEventListener(`resize`, (event) => {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-});
+
+function resize() {
+	const rect = canvas.getBoundingClientRect();
+	canvas.width = rect.width;
+	canvas.height = rect.height;
+}
+resize();
+window.addEventListener(`resize`, resize);
+
 const context = (() => {
 	const context = canvas.getContext(`2d`);
 	if (!context) {
@@ -42,7 +45,7 @@ const context = (() => {
 })();
 // context.translate(canvas.width / 2, canvas.height / 2);
 //#endregion
-//#region render
+//#region 
 /**
  * @param {String} text 
  * @param {Number} size 
@@ -69,7 +72,7 @@ function render(text, size) {
 }
 
 /** @type {Map<String, ImageData>} */ const library = new Map();
-const size = 50;
+const size = 100;
 const duration = 1;
 
 for (const char of text) {
@@ -78,17 +81,37 @@ for (const char of text) {
 	}
 }
 
+/** @type {Number?} */ let current = null;
+/** @type {Array<Array<Coordinate>>} */ const maps = [];
 const engine = new Engine(() => {
 	context.fillStyle = `#00000010`;
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	const phase = Math.floor(engine.time / (duration * 1000));
-	const timeout = (engine.time % (duration * 1000)) / (duration * 1000);
-	// rocket(canvas.width / 2, canvas.height / 2, duration);
-	const a = library.get(text[0]);
-	// console.log(a);
-	const b = deconstruct(a);
-	// console.log(b);
-	explosion(b, canvas.width / 2, canvas.height / 2);
+	if (current !== phase) {
+		maps.splice(0, maps.length, ...Array.from(text.split(`\s`)[0]).map((char) => {
+			const data = library.get(char);
+			if (!data) {
+				throw new ReferenceError(`Element 'data' isn't defined.`);
+			}
+			return map(data);
+		}));
+		current = phase;
+	}
+	let time = engine.time;
+	maps.forEach((map, index) => {
+		time -= index * 200;
+		const id = index + map.length * Math.floor(time - time % duration);
+		time = time % duration / duration;
+		let dx = (index + 1) * canvas.height / (1 + map.length);
+		dx += Math.min(0.33, time) * 100 * Math.sin(id);
+		let dy = canvas.height * 0.5;
+		dy += Math.sin(id * 4547.411) * canvas.height * 0.1;
+		if (time < 0.33) {
+			rocket(dx, dy, duration);
+		} else {
+			explosion(map, dx, dy);
+		}
+	});
 });
 
 //#region circle()
@@ -112,9 +135,9 @@ function circle(x, y, radius) {
  */
 function rocket(x, y, duration) {
 	context.fillStyle = `white`;
-	const time = (engine.time % (duration * 1000)) / (duration * 1000);
-	const radius = 2 * (1 - time + 8 * Math.pow(time, 16 * time));
-	circle(x, canvas.height - y * time, radius);
+	const cycle = engine.time % (duration * 1000) / (duration * 1000);
+	const radius = 2 * (1 - cycle + 8 * Math.pow(cycle, 16 * cycle));
+	circle(x, canvas.height - y * cycle, radius);
 }
 //#endregion
 //#region deconstruct()
@@ -126,59 +149,40 @@ function rocket(x, y, duration) {
 /**
  * @param {ImageData} image 
  */
-function deconstruct(image) {
-	/** @type {Array<Coordinate>} */ const particles = [];
+function map(image) {
+	/** @type {Array<Coordinate>} */ const chart = [];
 	if (image.data.some(unit => unit)) {
-		while (particles.length < 200) {
+		while (chart.length < 200) {
 			const x = image.width * Math.random();
 			const y = image.height * Math.random();
 			const offset = Math.floor(y) * image.height * 4 + Math.floor(x) * 4;
 			if (image.data[offset]) {
-				particles.push({ x: x - image.width / 2, y: y - image.height / 2 });
+				chart.push({ x: x - image.width / 2, y: y - image.height / 2 });
 			}
 		}
 	}
-	return particles;
+	return chart;
 }
 //#endregion
-
+//#region explosion()
 /**
  * @param {Array<Coordinate>} points 
  * @param {Number} xOffset 
  * @param {Number} yOffset 
  */
 function explosion(points, xOffset, yOffset) {
-	const T = Math.min(1, Math.max(0, (engine.time % duration / duration) - 0.33) * 2);
+	const cycle = engine.time % (duration * 1000) / (duration * 1000);
+	const T = Math.max(0, Math.min(cycle, 1));
 	const yDirty = (T * T * T) * 20;
-	let radius = 3;
+	let radius =/*  Math.sin(id) +  */3;
 	radius = T < 0.5 ? (T + 0.5) * T * radius : radius - T * radius;
-	context.fillStyle = `hsl(${55}, 55%, 55%)`;
+	context.fillStyle = `hsl(${/* id *  */55}, 55%, 55%)`;
 	points.forEach(({ x, y }, index) => {
-		if (index % 20 === 0) {
-			context.fillStyle = `hsl(${55}, 55%, ${55 + T * Math.sin(T * 55 + index) * 45}%)`;
+		if (index % 20 == 0) {
+			context.fillStyle = `hsl(${/* id *  */55}, 55%, ${55 + T * Math.sin(T * 55 + index) * 45}%)`;
 		}
-		circle(xOffset + T * x, canvas.height - yDirty + yOffset + T * y, radius);
+		circle(xOffset + T * x, yOffset + T * y - yDirty, radius);
 	});
 }
-
-/**
- * 
- * @param {Number} time 
- * @param {Number} index 
- * @param {Array<Array<Number>>} parts 
- */
-function firework(time, index, parts) {
-	time -= index * 200;
-	const id = index + chars.length * Math.floor(time - time % duration);
-	time = time % duration / duration;
-	let dx = (index + 1) * window.innerWidth / (1 + chars.length);
-	dx += Math.min(0.33, time) * 100 * Math.sin(id);
-	let dy = window.innerHeight * 0.5;
-	dy += Math.sin(id * 4547.411) * window.innerHeight * 0.1;
-	if (time < 0.33) {
-		rocket(dx, dy, time * 3);
-	} else {
-		explosion(parts, dx, dy, id, Math.min(1, Math.max(0, time - 0.33) * 2));
-	}
-}
+//#endregion
 //#endregion

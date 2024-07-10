4fcfa3ddf17a0bdaaf93e911680a3781d159adaa
Arman <eccs0103@gmail.com>
2024-07-10 15:02:03 +0400

    1.0.0

--- feed/script.js
+++ feed/script.js
@@ -2,6 +2,27 @@
 
 import { } from "../scripts/structure.js";
 
-await window.ensure(() => {
-	// Your code goes here
-});
+//#region Controller
+/**
+ * Represents the controller for the application.
+ */
+class Controller {
+	/**
+	 * Performs initial setup and loads necessary resources before the main logic.
+	 * @returns {Promise<void>}
+	 */
+	async awake() {
+		// Your code before loading goes here
+	}
+	/**
+	 * Contains the main logic for the application.
+	 * @returns {Promise<void>}
+	 */
+	async main() {
+		// Your code within loading goes here
+	}
+};
+const controller = new Controller();
+await window.assert(() => controller.awake());
+await window.assert(() => window.load(controller.main()));
+//#endregion

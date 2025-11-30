76b83bb81a5079c5a5eaa24075ecf433be5b7265
Arman <eccs0103@gmail.com>
2025-11-30 20:58:58 +0400

    2.0.0

--- feed/controllers/main-controller.ts
+++ feed/controllers/main-controller.ts
@@ -7,7 +7,7 @@ import database from "../data/activity.json";
 
 //#region Main controller
 class MainController extends Controller {
-	static addActivityLog(container: HTMLDivElement, activity: UserActivity): void {
+	static #addActivityLog(container: HTMLElement, activity: UserActivity): void {
 		const date = new Date(activity.timestamp).toLocaleString();
 
 		const divActivityLog = container.appendChild(document.createElement("div"));
@@ -25,7 +25,7 @@ class MainController extends Controller {
 	}
 
 	async run(): Promise<void> {
-		const divFeedContainer = await document.getElementAsync(HTMLDivElement, "div#feed-container");
+		const mainFeedContainer = await document.getElementAsync(HTMLElement, "main#feed-container");
 		const spanFooterYear = await document.getElementAsync(HTMLSpanElement, "span#footer-year");
 
 		const name = "activity";
@@ -35,7 +35,7 @@ class MainController extends Controller {
 		let limit = 5;
 		for (const activity of activities) {
 			if (limit <= 0) break;
-			MainController.addActivityLog(divFeedContainer, activity);
+			MainController.#addActivityLog(mainFeedContainer, activity);
 			limit--;
 		}
 

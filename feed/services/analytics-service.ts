989b43158b7793af8c0da04349a92ede3218d479
Arman <eccs0103@gmail.com>
2025-12-07 22:20:27 +0400

    2.0.0

--- feed/services/analytics-service.ts
+++ feed/services/analytics-service.ts
@@ -1,23 +1,21 @@
 "use strict";
 
-const id = "G-1N3MKL65T7";
-
 declare global {
 	export interface Window {
 		dataLayer: any[];
 	}
 }
 
-window.dataLayer = window.dataLayer || [];
-function gtag(...args: any[]): void {
-	window.dataLayer.push(args);
+const id = "G-1N3MKL65T7";
+
+function gtag(layer: any[], ...args: any[]): void {
+	layer.push(args);
 }
 
-gtag("js", new Date());
-gtag("config", id, { 'debug_mode': true });
+window.dataLayer = window.dataLayer || [];
+gtag(window.dataLayer, "js", new Date());
+gtag(window.dataLayer, "config", id);
 
 const script = document.head.appendChild(document.createElement("script"));
 script.async = true;
 script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
-
-console.log("Analytics initialized in Debug Mode");

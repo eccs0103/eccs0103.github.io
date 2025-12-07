c629b056a391c59fbd1bce43b84f788c06b58b7d
Arman <eccs0103@gmail.com>
2025-12-07 21:35:03 +0400

    2.0.0

--- feed/services/analytics-service.ts
+++ feed/services/analytics-service.ts
@@ -1,21 +1,29 @@
 "use strict";
 
+import "adaptive-extender/web";
+
+//#region Analytics service
 declare global {
 	export interface Window {
 		dataLayer: any[];
 	}
 }
 
-const id = "G-1N3MKL65T7";
+export class AnalyticsService {
+	constructor(id: string) {
+		window.dataLayer = window.dataLayer || [];
+		this.#gtag(window.dataLayer, "js", new Date());
+		this.#gtag(window.dataLayer, "config", id);
 
-function gtag(layer: any[], ...args: any[]) {
-	layer.push(args);
+		const script = document.head.appendChild(document.createElement("script"));
+		script.async = true;
+		script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
+	}
+
+	#gtag(layer: any[], ...args: any[]): void {
+		layer.push(args);
+	}
 }
+//#endregion
 
-window.dataLayer = window.dataLayer || [];
-gtag(window.dataLayer, "js", new Date());
-gtag(window.dataLayer, "config", id);
-
-const script = document.head.appendChild(document.createElement("script"));
-script.async = true;
-script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
+new AnalyticsService("G-1N3MKL65T7");

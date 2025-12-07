9908c3b24a3a3a855fe3db7f71c5b1e4a5af8d3e
Arman <eccs0103@gmail.com>
2025-12-07 21:06:24 +0400

    2.0.0

--- feed/services/analytics-service.ts
+++ feed/services/analytics-service.ts
@@ -2,10 +2,6 @@
 
 const id = "G-1N3MKL65T7";
 
-const script = document.head.appendChild(document.createElement("script"));
-script.async = true;
-script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
-
 declare global {
 	export interface Window {
 		dataLayer?: any[];
@@ -19,3 +15,7 @@ function gtag(layer: any[], ...args: any): void {
 window.dataLayer = window.dataLayer || [];
 gtag(window.dataLayer, "js", new Date());
 gtag(window.dataLayer, "config", id);
+
+const script = document.head.appendChild(document.createElement("script"));
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;

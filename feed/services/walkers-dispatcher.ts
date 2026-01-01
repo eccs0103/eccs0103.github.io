9674e65978ab3a5d0045f9d1141af31c78869289
Arman <eccs0103@gmail.com>
2026-01-01 04:47:58 +0400

    2.0.0

--- feed/services/walkers-dispatcher.ts
+++ feed/services/walkers-dispatcher.ts
@@ -26,10 +26,31 @@ export class ActivityDispatcher {
 	}
 
 	static async #runWalker(walker: ActivityWalker, since: Date, activities: Activity[]): Promise<void> {
+		const buffer: Activity[] = [];
+
 		for await (const target of walker.crawl(since)) {
-			if (activities.some(activity => Activity.isSame(activity, target))) continue;
-			activities.push(target);
+			buffer.push(target);
 		}
+
+		if (buffer.length > 0) {
+			const oldest = buffer.reduce((min, current) => current.timestamp.valueOf() < min.valueOf() ? current.timestamp : min, new Date(8640_000_000_000_000));
+			if (oldest.valueOf() > since.valueOf()) since = oldest;
+		}
+		let index = activities.length;
+		while (index--) {
+			const activity = activities[index];
+			if (activity.platform !== walker.name) continue;
+			if (activity.timestamp.valueOf() < since.valueOf()) continue;
+			const indexRemote = buffer.findIndex(remote => Activity.isSame(activity, remote));
+			if (indexRemote < 0) {
+				activities.splice(index, 1);
+				continue;
+			}
+			activities[index] = buffer[indexRemote];
+			buffer.splice(indexRemote, 1);
+		}
+
+		activities.push(...buffer);
 	}
 
 	static async #runWalkers(walkers: Iterable<ActivityWalker>, platforms: readonly Platform[], since: Date, activities: Activity[]): Promise<void> {
@@ -42,7 +63,7 @@ export class ActivityDispatcher {
 				await ActivityDispatcher.#runWalker(walker, since, activities);
 				const count = activities.length - before;
 				if (count === 0) continue;
-				console.log(`Added ${count} new activities from ${walker.name}`);
+				console.log(`Synced activities from ${walker.name}. Net change: ${count}`);
 			} catch (reason) {
 				console.error(`Unable to fetch activities from ${walker.name} cause:\n${Error.from(reason)}`);
 			}

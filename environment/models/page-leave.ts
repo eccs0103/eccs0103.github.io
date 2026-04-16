d1cd08a4911a4eaa5f7ebc7f9ce609be123b3fd5
Arman Sargsyan <arman.sargsyan2@nrgaming.com>
2026-04-16 16:22:01 +0400

    2.0.0

--- environment/models/page-leave.ts
+++ environment/models/page-leave.ts
@@ -6,8 +6,8 @@ import { Field, Model } from "adaptive-extender/core";
 //#region Page leave
 export class PageLeave extends Model {
 	/** GA4 native engagement time in milliseconds. Accumulated foreground-visible time for this page. GA4 uses this field to mark sessions as engaged (≥ 10 000 ms) and to populate "Average engagement time" in standard reports. */
-	@Field(Number, "engagement_time_ms")
-	engagementTimeMs: number;
+	@Field(Number, "engagement_time_msec")
+	engagementTimeMsec: number;
 
 	/** Total seconds the tab was in the foreground (visible state). Human-readable counterpart of engagement_time_msec for GA4 Explorer filters and BigQuery queries. Rounded to the nearest whole second. */
 	@Field(Number, "time_on_page")
@@ -26,7 +26,7 @@ export class PageLeave extends Model {
 		}
 
 		super();
-		this.engagementTimeMs = engagementTimeMsec;
+		this.engagementTimeMsec = engagementTimeMsec;
 		this.timeOnPage = timeOnPage;
 		this.maxScrollPercent = maxScrollPercent;
 	}
